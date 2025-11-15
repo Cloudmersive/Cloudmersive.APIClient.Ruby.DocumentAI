8989a7a2ff1d833a80a315d22bd48e8a1d218f04
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2025-11-15 05:35:32 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,11 +3,11 @@ Remove-Item *.gem
 Remove-Item ./client/*.gem
 
 
-Invoke-WebRequest -Uri 'https://api.cloudmersive.com/barcode/docs/v1/swagger' -OutFile '.\barcode-api-swagger.json'
-(Get-Content .\barcode-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\barcode-api-swagger.json
-(Get-Content .\barcode-api-swagger.json -Raw) -replace '"http"','"https"' | Set-Content .\barcode-api-swagger.json -Encoding UTF8
+Invoke-WebRequest -Uri 'https://api.cloudmersive.com/document-ai/docs/v1/swagger' -OutFile '.\documentai-api-swagger.json'
+(Get-Content .\documentai-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\documentai-api-swagger.json
+(Get-Content .\documentai-api-swagger.json -Raw) -replace '"http"','"https"' | Set-Content .\documentai-api-swagger.json -Encoding UTF8
 
-& java -jar swagger-codegen-cli-2.4.14.jar generate -i .\barcode-api-swagger.json -l ruby -o client -c packageconfig.json
+& java -jar swagger-codegen-cli-2.4.14.jar generate -i .\documentai-api-swagger.json -l ruby -o client -c packageconfig.json
 #(Get-Content ./client/src/api/ConvertDocumentApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertDocumentApi.js
 #(Get-Content ./client/src/api/ConvertWebApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertWebApi.js
 
